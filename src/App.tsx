dbb02bb0562c6ac41c37af3b9a6a5508e7c685b2
ronghuaww <wg.ronghua@gmail.com>
2026-06-03 19:12:56 -0500

    added buttons to carousel

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import TitleScreen from "./TitleScreen.tsx";
 import ProjectSection from "./ProjectSection.tsx";
 
 import tempImage from './assets/TempImage.jpg'
+import {Title} from "@mui/icons-material";
 
 
-const HIGHLIGHT_COLOR = "bg-teal-600/30"
+const HIGHLIGHT_COLOR = "bg-teal-700/20"
 
 const TitleCopy = {
-    "name": "Hello! \n I’m Andy Chang,",
+    "name": "Hello! I’m Andy Chang,",
     "githubLink": "https://github.com/andymjchang",
     "linkedinLink": "https://www.linkedin.com/in/andy-chang-0639a6262",
     "email":"andymjchang@gmail.com",
@@ -18,10 +19,40 @@ const TitleCopy = {
         tempImage]
 }
 
+const DumpsterDiverCopy = {
+    "title": "Vegetables of\nMass Destruction",
+    "subtitle": "Gameplay Engineer / C++, Unreal Engine 5, Blueprints",
+    "description": "Andy Chang",
+    "image": tempImage,
+    "subsections": [
+        {
+            "description": "Designed and implemented 3 weapon classes, 7 enemies, and 20+ character upgrades",
+            "image": tempImage,
+        },
+        {
+            "description": "Built state/behavior trees for the combat systems, character animations, and complex NPC AI",
+            "image": tempImage,
+        },
+        {
+            "description": "Optimized for desktop/mobile platforms using Unreal Insights to profile and identify frame‑rate bottlenecks",
+            "image": tempImage,
+        },
+        {
+            "description": "Established weekly playtest feedback loops, boosting playtester ratings by 40% over 4 months",
+            "image": tempImage,
+        },
+        {
+            "description": "Shipped to Steam with over 28,000 downloads",
+            "image": tempImage,
+        },
+    ]
+}
+
 function Subtitle() {
     return (
         <div className="text-center">
-            a <a className="font-bold animate-highlight"> Gameplay Engineer </a>
+            a <a className="font-bold animate-highlight">
+            Gameplay Engineer </a>
             with a focus on <a className={HIGHLIGHT_COLOR}>optimization</a>
             ,<a className={HIGHLIGHT_COLOR}> tool development</a>
             , and <a className={HIGHLIGHT_COLOR}>intelligent AI entities</a>.
@@ -43,7 +74,13 @@ function App() {
           <Subtitle/>
       </TitleScreen>
 
-      <ProjectSection/>
+      <ProjectSection
+          title={DumpsterDiverCopy.title}
+          subtitle={DumpsterDiverCopy.subtitle}
+          description={DumpsterDiverCopy.description}
+          image={DumpsterDiverCopy.image}
+          subsections={DumpsterDiverCopy.subsections}
+      />
 
     </>
   )
--- src/ProjectSection.tsx
+++ src/ProjectSection.tsx
@@ -1,116 +1,131 @@
-import { useState } from 'react'
 import tempImage from './assets/TempImage.jpg'
-import Sticky from 'react-stickynode'; // react-stickynode to handle sticky states
 
 import ArrowBackRoundedIcon from '@mui/icons-material/ArrowBackRounded';
 import ArrowForwardRoundedIcon from '@mui/icons-material/ArrowForwardRounded';
 
 
 import "./app.css";
-import {AppBar, Slide, Toolbar, useScrollTrigger} from "@mui/material";
+import {type MouseEventHandler, useLayoutEffect, useRef, useState} from "react";
+import {IconButton} from "@mui/material";
 
+type ProjectTitleContainerProps = {
+    "title": string;
+    "subtitle": string;
+    "description": string;
+    "image": string;
+}
 
-type statusData = {
-    status: number;
-};
+type subsectionsProps = {
+    "description": string;
+    "image": string;
+}
 
-function HideOnScroll() {
-    const trigger = useScrollTrigger();
+type ProjectSubsectionProps = {
+    "subsections": subsectionsProps[]
+}
+
+type ProjectSectionProps = ProjectTitleContainerProps & ProjectSubsectionProps
+
+function ProjectTitleContainer({ title, subtitle, description, image }: ProjectTitleContainerProps) {
     return (
-        <Slide in={!trigger}>
-            <header className="bg-gray-900">
-                Andy Chang / Gameplay Engineer
-            </header>
-        </Slide>
-    );
+        <>
+            <div className="h-140">
+                <img className="h-full w-full object-cover"
+                     src={tempImage} alt={"Cover"}/>
+            </div>
+            <div className="px-16 pt-8 text-left">
+                <h1 className="
+                                font-extrabold tracking-tighter
+                                text-5xl md:text-6xl lg:text-7xl
+                                whitespace-pre-wrap">
+                    {title}
+                </h1>
+                <h2 className="py-2 text-2xl font-light">
+                    {subtitle}
+                </h2>
+
+                <p className="py-5 text-base">
+                    {description}
+                </p>
+            </div>
+        </>
+    )
 }
 
 
-function ProjectSection() {
-    const [isTitleReleased, setIsTitleReleased] = useState(false);
+function ProjectSubsectionsContainer({ subsections }: ProjectSubsectionProps) {
+    const carouselRef  = useRef<HTMLDivElement | null>(null);
+    const cardRef  = useRef<HTMLDivElement | null>(null);
+    const [cardWidth, setCardWidth] = useState(0);
 
-    const handleStateChange = (status: statusData) => {
-        if (status.status === Sticky.STATUS_RELEASED) {
-            console.log(status);
-            setIsTitleReleased(true);
+
+    useLayoutEffect(() => {
+        if (cardRef.current) {
+            // Call getBoundingClientRect() to fetch layout metrics
+            const rect = cardRef.current?.getBoundingClientRect();
+            setCardWidth(rect.width);
+        }
+    }, []);
+
+
+    const handleLeftClick: React.MouseEventHandler<HTMLButtonElement> = (e) => {
+        if (carouselRef.current) {
+            console.log("moving to the left");
+            carouselRef.current.scrollBy({ left: -cardWidth, behavior: "smooth" });
         }
     }
 
-    const handleFreeze = () => {
-        return isTitleReleased;
-    };
+    const handleRightClick: React.MouseEventHandler<HTMLButtonElement> = (e) => {
+        if (carouselRef.current) {
+            console.log("moving to the right");
+            carouselRef.current.scrollBy({ left: cardWidth, behavior: "smooth" });
+        }
+    }
 
+    return (
+        <div className="">
+            <div ref={carouselRef}
+                 className="snap-x snap-proximity flex flex-row overflow-x-scroll">
+
+                {subsections.map((item, index) => (
+                    <div ref={cardRef} key={index}>
+                        <div className={`snap-center flex-none m-3 w-xs ${index == 0 ? 'ml-16' : ''}`}>
+                            <img className="object-cover rounded-xl h-60"
+                                 src={item.image} alt={"Cover"}/>
+                            <p className="">
+                                {item.description}
+                            </p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className="">
+                <IconButton onClick={handleLeftClick}>
+                    <ArrowBackRoundedIcon/>
+                </IconButton>
+                <IconButton onClick={handleRightClick}>
+                    <ArrowForwardRoundedIcon/>
+                </IconButton>
+            </div>
+        </div>
+    )
+}
+
+
+function ProjectSection({ title, subtitle, description, image, subsections}
+                        : ProjectSectionProps) {
 
     return (
         <div className="">
 
-            <div className="relative w-full h-96 md:h-156 ">
+            <ProjectTitleContainer
+            title={title}
+            subtitle={subtitle}
+            description={description}
+            image={image}/>
 
-                <div className="relative h-full w-full">
-                    <img className="absolute h-full w-full object-cover"
-                         src={tempImage} alt={"Cover"}/>
-                    {/*<div className="absolute h-full w-full bg-linear-to-b from-transparent from-80% to-white "></div>*/}
-
-                </div>
-                <h1 className="absolute w-full left-0 font-extrabold tracking-tighter text-left
-                                px-16
-                                -bottom-6 md:-bottom-7 lg:-bottom-8
-                                text-5xl md:text-6xl lg:text-7xl stroke-white">
-                   <a className="bg-mist-100/30"> Vegetables of <br/> Mass Destruction </a>
-                </h1>
-            </div>
-            <h2 className="pt-8 md:pt-9 lg:pt-10
-                            px-16
-                            w-full left-0  text-2xl font-light text-left">
-                Gameplay Engineer / C++, Unreal Engine 5, Blueprints
-            </h2>
-
-            <p className="pt-2 md:pt-3 lg:pt-4
-                            px-16
-                            w-full left-0 text-base text-left">
-                something about what the game is about, something about what the game is about
-
-            </p>
-
-
-
-
-
-            <div className="flex flex-row pl-16 overflow-x-scroll">
-
-                <div className="flex-none m-2 pl-0 w-xs">
-                    <img className=" object-cover rounded-xl h-60"
-                         src={tempImage} alt={"Cover"}/>
-                    <p className="">
-                        ctfghjbnbvcftuyguhjbn vjcgfygkuhj
-                        knbvkfiyt7ouyhbjkhvkghiu
-                        yhibjvjkfyigt7y8ihukbn
-                    </p>
-                </div>
-                <div className="flex-none m-2 w-xs">
-                    <img className="object-cover rounded-xl h-60"
-                         src={tempImage} alt={"Cover"}/>
-                    <p className="">
-                        ctfghjbnbvc ftuyguhjbn vjcgf ygkuhj
-                        knbvk fiyt7ouyhbj khvkghiu
-                        yhibjvjkfyigt7y8ihukbn
-                    </p>
-                </div>
-                <div className="flex-none m-2 w-xs">
-                    <img className="object-cover rounded-xl h-60"
-                         src={tempImage} alt={"Cover"}/>
-                    <p className="">
-                        ctfghjbn bvc ftuy guhjbn vjcgfygkuhj
-                        knbvkf iy t7ouy hbjk hvk ghiu
-                        yhibjvjk fyigt 7y8ih uk bn
-                    </p>
-                </div>
-            </div>
-            <div className="">
-                <ArrowBackRoundedIcon/>
-                <ArrowForwardRoundedIcon/>
-            </div>
-
+            <ProjectSubsectionsContainer
+            subsections={subsections}/>
         </div>
     )
 }
